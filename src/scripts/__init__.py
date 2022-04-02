c89ca6ec972a491a7d1c610e3988a45964970fe4
Austhopia <fathurrahmansyarief@gmail.com>
2022-04-02 16:49:43 +0700

    add database

--- src/scripts/__init__.py
+++ src/scripts/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from random import randint
 from datetime import datetime
 
 UPLOAD_FOLDER = "scripts\static"
@@ -15,40 +14,18 @@ app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
     }
 }
 
-def random_integer():
-    min_ = 100
-    max_ = 1000000
-    rand = randint(min_, max_)
-
-    return int(rand)
-
 db = SQLAlchemy(app)
 
-db.session.execute(
-    'CREATE TABLE IF NOT EXISTS blog (id INTEGER, title VARCHAR(200) NOT NULL, author VARCHAR(100) NOT NULL, blog_text VARCHAR(100000), PRIMARY KEY(id))'
-    )
-# db.session.execute('CREATE TABLE IF NOT EXISTS clients (id INTEGER NOT NULL,"user_id"	INTEGER, username VARCHAR(100) NOT NULL,	date_added DATETIME, PRIMARY KEY (id))')
+class Blogs(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    img = db.Column(db.Text, nullable=True)
+    img_base64 = db.Column(db.Text, nullable=True)
+    img_name = db.Column(db.Text, nullable=True)
+    mimetype = db.Column(db.Text, nullable=True)
+    blog_content = db.Column(db.Text, nullable=False)
+    posted_on = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
 
-db.session.close()
 
-class blog(db.Model):
-    __table__name = 'blog',
-    id = db.Column(db.Integer, primary_key=True),
-    title = db.Column(db.String(100), nullable=False),
-    author = db.Column(db.String(100), nullable=False),
-    blog_text = db.Column(db.String(100000), nullable=True)
-    def __repr__(self):
-        return f'{self.id} - {self.title} - {self.author} - {self.blog_text} - {self.thumbnail}'
-# class Clients(db.Model):
-#     __tablename__ = 'clients'
-#     id = db.Column(db.Integer, primary_key=True)
-#     user_id = db.Column(db.Integer, default=random_integer, primary_key=True)
-#     username = db.Column(db.String(100), nullable=False)
-#     date_added = db.Column(db.DateTime, default=datetime.utcnow)
-#     clients_data = db.relationship('Clients_Data', backref='author', lazy=True)
-#     clients_input = db.relationship('Clients_Input', backref='author', lazy=True)
-
-#     def __repr__(self):
-#         return f'{self.user_id} - {self.username} - {self.date_added}'
-        
 from scripts import routes
--- src/scripts/routes.py
+++ src/scripts/routes.py
@@ -1,28 +1,38 @@
-from flask import flash, redirect, render_template, request
+from email.policy import default
+from io import BytesIO
+from flask import flash, redirect, render_template, request, send_file
 from flask.helpers import url_for
 from werkzeug.utils import secure_filename
 from scripts import app
 import os
-
+from scripts import Blogs, db
+from sqlalchemy import desc
+import base64
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+def render_picture(data):
+    render_pic = base64.b64encode(data).decode('ascii') 
+    return render_pic
 
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route("/")
-@app.route("/home")
+@app.route("/", methods=['GET', 'POST'])
+@app.route("/home", methods=['GET', 'POST'])
 def index_page():
-    return render_template('index.html')
+    if request.method == 'POST':
+        flash('hello')
+        return render_template('write.html')
+
+    all_post = Blogs.query.order_by(Blogs.posted_on).all()
+    return render_template('index.html', posts=all_post)
 
 
-@app.route("/author")
-def author_page():
-    return render_template('author.html')
-
-
-@app.route("/post")
+@app.route("/post", methods=['GET', 'POST'])
 def post_page():
+    # post_id = db.session.query(Blogs.id).order_by(desc(Blogs.posted_on)).first()
+
     return render_template('post.html')
 
 
@@ -32,24 +42,36 @@ def write_page():
         _title = request.form.get("title")
         _author = request.form.get("author")
         _blog = request.form.get("blog_content")
-        db.session.add(blog())
         print(_title, _author, _blog)
 
-        Upload Thumbnail
+        # Storing Thumbnail
         if 'file' not in request.files:
             flash("no file part")
             return redirect(request.url)
+
         file = request.files['file']
+
         if file.filename == '':
             flash('No Image selected for uploading')
             return redirect(request.url)
+
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'] , filename))
+            mimetype = file.mimetype
+
+            if not filename or not mimetype:
+                return '400'
+
+            _img_base64 = render_picture(file.read())
+
+            blog = Blogs(title=_title, author=_author, img=file.read(), img_base64=_img_base64, img_name=filename, mimetype=mimetype, blog_content=_blog)
+            db.session.add(blog)
+            db.session.commit()
+
             flash("Image Succesfuly")
             return render_template('write.html')
         else:
             flash("Error")
-            return redirect(request.url)
         
     return render_template('write.html')
+
